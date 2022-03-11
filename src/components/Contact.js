0c9faaa8bbafaa044b6e941ecb324459037b8e30
bitopan987 <bitopanxon1@gmail.com>
2022-03-11 14:36:47 +0530

    refactor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -109,8 +109,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                AltCampus, Dharamshala <br />
-                Himachal Pradesh, India
+                Guwahati,Assam <br />
+                India
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
